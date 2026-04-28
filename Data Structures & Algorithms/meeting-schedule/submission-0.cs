2f36a1c43cae63699131ae4bd96b90c7fda4fdec
orrest <84518085+orrest@users.noreply.github.com>
2026-04-28 10:33:23 +0800

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool CanAttendMeetings(List<Interval> intervals) {
+        intervals.Sort((i1, i2) => i1.start.CompareTo(i2.start));
+
+        for (int i = 1; i < intervals.Count; i++) {
+            Interval i1 = intervals[i - 1];
+            Interval i2 = intervals[i];
+
+            if (i1.end > i2.start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
